2b30c6498f7f0333b92c2c675a9fbf3f1219bca6
Chad Jessup <chad.jessup@gmail.com>
2019-08-01 22:43:12 -0700

    Initial Commit

---
+++ Kafka.Streams.net/Kafka.Streams.App/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Kafka.Streams.App
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Kafka.Streams.net/Kafka.Streams/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Kafka.Streams
+{
+    public class Class1
+    {
+    }
+}
